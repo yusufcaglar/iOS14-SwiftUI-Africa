f3043eb3790a9d917a64d9a09d564cdce005276e
Yusuf Çağlar <faf7faf7@gmail.com>
2021-03-26 19:52:38 +0300

    Initial Commit

---
+++ Africa/AfricaApp.swift
@@ -0,0 +1,17 @@
+//
+//  AfricaApp.swift
+//  Africa
+//
+//  Created by Yusuf ÇAĞLAR on 26/03/2021.
+//
+
+import SwiftUI
+
+@main
+struct AfricaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
