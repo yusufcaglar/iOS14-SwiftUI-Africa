c30b4f460966eb274d23d59b7e2bf467b271d573
Yusuf Çağlar <faf7faf7@gmail.com>
2021-03-29 15:32:24 +0300

    Add AnimalGridItemView and add a complex Grid layout system in ContentView.

--- Africa/App/ContentView.swift
+++ Africa/App/ContentView.swift
@@ -9,23 +9,100 @@ import SwiftUI
 
 struct ContentView: View {
     //MARK: - Properties
+    
     let animals: [Animal] = Bundle.main.decode("animals.json")
+    let haptics = UIImpactFeedbackGenerator(style: .medium)
+    
+    @State private var isGridViewActive: Bool = false
+    
+    @State private var gridLayout: [GridItem] = [GridItem(.flexible())]
+    @State private var gridColumn: Int = 1
+    @State private var toolbarIcon: String = "square.grid.2x2"
+    
+    //MARK: - Functions
+    
+    func gridSwitch() {
+        gridLayout = Array(repeating: .init(.flexible()), count: gridLayout.count % 3 + 1)
+        gridColumn = gridLayout.count
+        print("Grid Number: \(gridColumn)")
+        
+        // Toolbar Image
+        
+        switch gridColumn {
+        case 1:
+            toolbarIcon = "square.grid.2x2"
+        case 2:
+            toolbarIcon = "square.grid.3x2"
+        case 3:
+            toolbarIcon = "rectangle.grid.1x2"
+        default:
+            toolbarIcon = "square.grid.2x2"
+        }
+    }
     
     //MARK: - Body
     var body: some View {
         NavigationView {
-            List {
-                CoverImageView()
-                    .frame(height: 300)
-                    .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
-                
-                ForEach(animals) { animal in
-                    NavigationLink(destination: AnimalDetailView(animal: animal)) {
-                        AnimalListItemView(animal: animal)
-                    } //: NavigationLink
-                } //: Loop
-            } //: List
+            
+            Group {
+                if !isGridViewActive {
+                    List {
+                        CoverImageView()
+                            .frame(height: 300)
+                            .listRowInsets(EdgeInsets(top: 0, leading: 0, bottom: 0, trailing: 0))
+                        
+                        ForEach(animals) { animal in
+                            NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                AnimalListItemView(animal: animal)
+                            } //: NavigationLink
+                        } //: Loop
+                    } //: List
+                } else {
+                    ScrollView(.vertical, showsIndicators: false) {
+                        LazyVGrid(columns: gridLayout, alignment: .center, spacing: 10) {
+                            ForEach(animals) { animal in
+                                NavigationLink(destination: AnimalDetailView(animal: animal)) {
+                                    AnimalGridItemView(animal: animal)
+                                } //: Link
+                            } //: Loop
+                        } //: LazyVGrid
+                        .padding(10)
+                        .animation(.easeIn)
+                    } //: ScrollView
+                } //: Condition
+            } //: Group
+            
             .navigationBarTitle("Africa", displayMode: .large)
+            .toolbar {
+                ToolbarItem(placement: .navigationBarTrailing) {
+                    HStack(spacing: 16) {
+                        // List
+                        Button(action: {
+                            print("List view is activated.")
+                            isGridViewActive = false
+                            haptics.impactOccurred()
+                        }) {
+                            Image(systemName: "square.fill.text.grid.1x2")
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .primary : .accentColor)
+                        }
+                        
+                        // Grid
+                        
+                        Button(action: {
+                            print("Grid view is activated.")
+                            isGridViewActive = true
+                            haptics.impactOccurred()
+                            gridSwitch()
+                        }) {
+                            Image(systemName: toolbarIcon)
+                                .font(.title2)
+                                .foregroundColor(isGridViewActive ? .accentColor : .primary)
+                        }
+                        
+                    } //: HStack
+                } //: Buttons
+            } //: Toolbar
         } //: NavigationView
     }
 }
